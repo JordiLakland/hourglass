9e43177acb317d5ddfa2177b1661e81fb4b1f1ea
JordiLakland <developsuricata@gmail.com>
2021-11-25 11:44:04 +0100

    Create HourGlass Frontent App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HourGlass APP
+      </header>
+      <div className="main-content">
+        This is my App Content
+      </div>
+    </div>
+  );
+}
+
+export default App;
